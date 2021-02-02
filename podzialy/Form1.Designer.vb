8820546fefbb77b327aee1be673b093dd3dc202c
mar kom <m4nk9i@gmail.com>
2021-02-02 12:53:03 +0100

    wersja 1.0 na koniec dnia

--- podzialy/Form1.Designer.vb
+++ podzialy/Form1.Designer.vb
@@ -22,10 +22,323 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox5 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox6 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox7 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox8 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox9 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "format arkusza"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(328, 35)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(328, 87)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "format użytka"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(68, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "wycinki"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(129, 237)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(28, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "lewy"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(213, 200)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(33, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "górny"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(325, 238)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(35, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "prawy"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(214, 281)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "dolny"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(24, 339)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "przelicz"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(116, 35)
+        Me.MaskedTextBox1.Mask = "00000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox1.TabIndex = 20
+        Me.MaskedTextBox1.ValidatingType = GetType(Integer)
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(222, 35)
+        Me.MaskedTextBox2.Mask = "00000"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox2.TabIndex = 21
+        '
+        'MaskedTextBox3
+        '
+        Me.MaskedTextBox3.Location = New System.Drawing.Point(116, 86)
+        Me.MaskedTextBox3.Mask = "00000"
+        Me.MaskedTextBox3.Name = "MaskedTextBox3"
+        Me.MaskedTextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox3.TabIndex = 22
+        '
+        'MaskedTextBox4
+        '
+        Me.MaskedTextBox4.Location = New System.Drawing.Point(222, 87)
+        Me.MaskedTextBox4.Mask = "00000"
+        Me.MaskedTextBox4.Name = "MaskedTextBox4"
+        Me.MaskedTextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox4.TabIndex = 23
+        '
+        'MaskedTextBox5
+        '
+        Me.MaskedTextBox5.Location = New System.Drawing.Point(116, 135)
+        Me.MaskedTextBox5.Mask = "00000"
+        Me.MaskedTextBox5.Name = "MaskedTextBox5"
+        Me.MaskedTextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox5.TabIndex = 24
+        Me.MaskedTextBox5.Text = "4"
+        '
+        'MaskedTextBox6
+        '
+        Me.MaskedTextBox6.Location = New System.Drawing.Point(252, 193)
+        Me.MaskedTextBox6.Mask = "00000"
+        Me.MaskedTextBox6.Name = "MaskedTextBox6"
+        Me.MaskedTextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox6.TabIndex = 25
+        Me.MaskedTextBox6.Text = "10"
+        '
+        'MaskedTextBox7
+        '
+        Me.MaskedTextBox7.Location = New System.Drawing.Point(252, 274)
+        Me.MaskedTextBox7.Mask = "00000"
+        Me.MaskedTextBox7.Name = "MaskedTextBox7"
+        Me.MaskedTextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox7.TabIndex = 26
+        Me.MaskedTextBox7.Text = "15"
+        '
+        'MaskedTextBox8
+        '
+        Me.MaskedTextBox8.Location = New System.Drawing.Point(163, 231)
+        Me.MaskedTextBox8.Mask = "00000"
+        Me.MaskedTextBox8.Name = "MaskedTextBox8"
+        Me.MaskedTextBox8.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox8.TabIndex = 27
+        Me.MaskedTextBox8.Text = "10"
+        '
+        'MaskedTextBox9
+        '
+        Me.MaskedTextBox9.Location = New System.Drawing.Point(366, 230)
+        Me.MaskedTextBox9.Mask = "00000"
+        Me.MaskedTextBox9.Name = "MaskedTextBox9"
+        Me.MaskedTextBox9.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox9.TabIndex = 28
+        Me.MaskedTextBox9.Text = "10"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(34, 201)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(54, 13)
+        Me.Label8.TabIndex = 29
+        Me.Label8.Text = "marginesy"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(24, 368)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(439, 310)
+        Me.TextBox1.TabIndex = 30
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(489, 35)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(498, 643)
+        Me.Panel1.TabIndex = 31
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(35, 231)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 32
+        Me.Button2.Text = "zero"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(35, 260)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 33
+        Me.Button3.Text = "zwykłe"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(233, 132)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 34
+        Me.Button4.Text = "ustaw na 0"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(314, 132)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 35
+        Me.Button5.Text = "ustaw na 4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1013, 704)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.MaskedTextBox9)
+        Me.Controls.Add(Me.MaskedTextBox8)
+        Me.Controls.Add(Me.MaskedTextBox7)
+        Me.Controls.Add(Me.MaskedTextBox6)
+        Me.Controls.Add(Me.MaskedTextBox5)
+        Me.Controls.Add(Me.MaskedTextBox4)
+        Me.Controls.Add(Me.MaskedTextBox3)
+        Me.Controls.Add(Me.MaskedTextBox2)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Podziały arkusza"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents MaskedTextBox3 As MaskedTextBox
+    Friend WithEvents MaskedTextBox4 As MaskedTextBox
+    Friend WithEvents MaskedTextBox5 As MaskedTextBox
+    Friend WithEvents MaskedTextBox6 As MaskedTextBox
+    Friend WithEvents MaskedTextBox7 As MaskedTextBox
+    Friend WithEvents MaskedTextBox8 As MaskedTextBox
+    Friend WithEvents MaskedTextBox9 As MaskedTextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
 End Class
--- podzialy/Form1.vb
+++ podzialy/Form1.vb
@@ -1,3 +1,199 @@
 ﻿Public Class Form1
 
+    Private graf As Graphics
+    Dim szerokosc_ark As Single
+    Dim wysokosc_ark As Single
+    Dim szerokosc_uz As Single
+    Dim wysokosc_uz As Single
+    Dim wycinki As Single
+    Dim marg_gora As Single
+    Dim marg_lewa As Single
+    Dim marg_dol As Single
+    Dim marg_prawa As Single
+
+    Dim pole_wys As Single
+    Dim pole_szer As Single
+    Dim liczba_wersow1 As Integer
+    Dim liczba_kolumn1 As Integer
+    Dim liczba_wersow2 As Integer
+    Dim liczba_kolumn2 As Integer
+
+
+
+
+    Dim formaty_pap = {
+        {"430", "305", "A3"},
+        {"500", "350", "B3"},
+        {"610", "430", "A2"},
+        {"630", "440", "A2+"},
+        {"640", "450", "C2"},
+        {"640", "460", "C2 większe"},
+        {"660", "485", "MAX"},
+        {"610", "286", "kiszka 610"},
+        {"650", "350", "kiszka 650"},
+        {"660", "330", "kiszka 660"},
+        {"700", "500", "B2"}
+    }
+
+    Dim formaty_uz = {
+    {"90", "50", "wizytowka"},
+    {"210", "148", "A5"},
+    {"297", "210", "A4"},
+    {"250", "175", "B5"},
+    {"350", "250", "B4"},
+    {"210", "99", "DL"}
+    }
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+        For index0 = 0 To formaty_pap.GetUpperBound(0)
+            ComboBox1.Items.Add(formaty_pap(index0, 2) + "(" + formaty_pap(index0, 0) + "x" + formaty_pap(index0, 1) + ")")
+
+        Next
+
+
+        For index0 = 0 To formaty_uz.GetUpperBound(0)
+            ComboBox2.Items.Add(formaty_uz(index0, 2) + "(" + formaty_uz(index0, 0) + "x" + formaty_uz(index0, 1) + ")")
+
+        Next
+
+        ComboBox1.SelectedIndex = 0
+        ComboBox2.SelectedIndex = 0
+
+        graf = Panel1.CreateGraphics
+        policz()
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        MaskedTextBox1.Text = formaty_pap(ComboBox1.SelectedIndex, 0)
+        MaskedTextBox2.Text = formaty_pap(ComboBox1.SelectedIndex, 1)
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+        MaskedTextBox3.Text = formaty_uz(ComboBox2.SelectedIndex, 0)
+        MaskedTextBox4.Text = formaty_uz(ComboBox2.SelectedIndex, 1)
+
+
+    End Sub
+
+    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs)
+
+    End Sub
+
+    Private Sub prostokat(kol As Pen, px As Integer, py As Integer, pw As Integer, ph As Integer)
+        Dim skala = 0.65
+        graf.DrawRectangle(kol, CInt(px * skala), CInt(py * skala), CInt(pw * skala), CInt(ph * skala))
+
+    End Sub
+
+    Private Sub policz()
+
+        szerokosc_ark = CDbl(MaskedTextBox1.Text)
+        wysokosc_ark = CDbl(MaskedTextBox2.Text)
+        szerokosc_uz = CDbl(MaskedTextBox3.Text)
+        wysokosc_uz = CDbl(MaskedTextBox4.Text)
+        wycinki = CDbl(MaskedTextBox5.Text)
+        marg_gora = CDbl(MaskedTextBox6.Text)
+        marg_lewa = CDbl(MaskedTextBox8.Text)
+        marg_dol = CDbl(MaskedTextBox7.Text)
+        marg_prawa = CDbl(MaskedTextBox9.Text)
+
+
+        pole_wys = wysokosc_ark - marg_dol - marg_gora
+        pole_szer = szerokosc_ark - marg_lewa - marg_prawa
+        liczba_wersow1 = Math.Floor((pole_wys + wycinki) / (wysokosc_uz + wycinki))
+        liczba_kolumn1 = Math.Floor((pole_szer + wycinki) / (szerokosc_uz + wycinki))
+        liczba_wersow2 = Math.Floor((pole_wys + wycinki) / (szerokosc_uz + wycinki))
+        liczba_kolumn2 = Math.Floor((pole_szer + wycinki) / (wysokosc_uz + wycinki))
+
+
+        TextBox1.AppendText("wyliczenia dla arkusza " + CStr(szerokosc_ark) + " x " + CStr(wysokosc_ark) + System.Environment.NewLine)
+        TextBox1.AppendText("pole zadruku z uzglednieniem marginesow: " + CStr(pole_szer) + " x " + CStr(pole_wys) + System.Environment.NewLine)
+        TextBox1.AppendText("uzytek " + CStr(szerokosc_uz) + " x " + CStr(wysokosc_uz) + " wycinki " + CStr(wycinki) + System.Environment.NewLine)
+        TextBox1.AppendText("liczba uzytkow " + CStr(liczba_wersow1) + " x " + CStr(liczba_kolumn1) + " = " + CStr(liczba_kolumn1 * liczba_wersow1) + System.Environment.NewLine)
+        TextBox1.AppendText("liczba uzytkow " + CStr(liczba_wersow2) + " x " + CStr(liczba_kolumn2) + " = " + CStr(liczba_kolumn2 * liczba_wersow2) + System.Environment.NewLine)
+
+
+
+    End Sub
+
+    Private Sub rysuj()
+        graf.Clear(Color.White)
+
+        prostokat(Pens.Black, 0, 0, szerokosc_ark, wysokosc_ark)
+
+        prostokat(Pens.Black, 0, (wysokosc_ark), (szerokosc_ark), CInt(wysokosc_ark))
+        prostokat(Pens.Green, marg_lewa, marg_gora, szerokosc_ark - marg_prawa - marg_lewa, wysokosc_ark - marg_dol - marg_gora)
+        prostokat(Pens.Green, marg_lewa, wysokosc_ark + marg_gora, szerokosc_ark - marg_prawa - marg_lewa, wysokosc_ark - marg_dol - marg_gora)
+
+        If ((liczba_kolumn1 > 0) And (liczba_wersow1 > 0)) Then
+            Dim startx = marg_lewa + (pole_szer - (liczba_kolumn1 * szerokosc_uz + (liczba_kolumn1 - 1) * wycinki)) / 2
+            Dim starty = marg_gora + (pole_wys - (liczba_wersow1 * wysokosc_uz + (liczba_wersow1 - 1) * wycinki)) / 2
+            For i As Integer = 0 To liczba_kolumn1 - 1
+                For j As Integer = 0 To liczba_wersow1 - 1
+                    prostokat(Pens.Black, CInt(startx + i * (szerokosc_uz + wycinki)), CInt(starty + j * (wysokosc_uz + wycinki)), szerokosc_uz, wysokosc_uz)
+                Next j
+            Next i
+        End If
+
+
+        If ((liczba_kolumn2 > 0) And (liczba_wersow2 > 0)) Then
+            Dim startx = marg_lewa + (pole_szer - (liczba_kolumn2 * wysokosc_uz + (liczba_kolumn2 - 1) * wycinki)) / 2
+            Dim starty = wysokosc_ark + marg_gora + (pole_wys - (liczba_wersow2 * szerokosc_uz + (liczba_wersow2 - 1) * wycinki)) / 2
+            For i As Integer = 0 To liczba_kolumn2 - 1
+                For j As Integer = 0 To liczba_wersow2 - 1
+                    prostokat(Pens.Black, CInt(startx + i * (wysokosc_uz + wycinki)), CInt(starty + j * (szerokosc_uz + wycinki)), wysokosc_uz, szerokosc_uz)
+                Next j
+            Next i
+        End If
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        policz()
+        rysuj()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        rysuj()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MaskedTextBox6.Text = "0"
+        MaskedTextBox7.Text = "0"
+        MaskedTextBox8.Text = "0"
+        MaskedTextBox9.Text = "0"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MaskedTextBox6.Text = "10"
+        MaskedTextBox7.Text = "15"
+        MaskedTextBox8.Text = "10"
+        MaskedTextBox9.Text = "10"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MaskedTextBox5.Text = "0"
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        MaskedTextBox5.Text = "4"
+    End Sub
 End Class
